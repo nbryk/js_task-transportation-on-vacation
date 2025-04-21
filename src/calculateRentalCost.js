876aca9f9aeb4d0d6179e0a74a1b32df45004855
Nadiia Bryk <25nadin47@gmail.com>
2025-04-21 18:55:37 +0300

    changed names

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,20 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const numberOfDays = days;
-  const basePrice = 40;
-  const totalAmount = numberOfDays * basePrice;
-
+  const BASE_PRICE = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
+  const totalAmount = days * BASE_PRICE;
 
-  if (numberOfDays >= LONG_TERM) {
+  if (days >= LONG_TERM) {
     return totalAmount - LONG_TERM_DISCOUNT;
   }
 
-  if (numberOfDays >= SHORT_TERM) {
+  if (days >= SHORT_TERM) {
     return totalAmount - SHORT_TERM_DISCOUNT;
   }
 
